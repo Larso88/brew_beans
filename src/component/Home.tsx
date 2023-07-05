ee6f676ba98e24a15645e8c18673aa087a3d7f0b
Lars Ole Algrøy <lars_ole_algroy@outlook.com>
2023-07-06 00:22:38 +0200

    - Created home page and added about page. - Styled both home page and about page. - Added a footer with relevant info.

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,11 +1,21 @@
 import React, { FunctionComponent } from 'react';
-import "./home.css"
+import "./home.css";
+import CoffeShopImage from "../images/coffeShopHero.jpg"
+import { Link } from 'react-router-dom';
 
 
 
 const Home: FunctionComponent = () => {
     return ( 
-        <p>Home</p>
+        <div className='heroImageDiv'>
+            <img className='heroImage' src={CoffeShopImage} alt="" />
+            <h1 className='heroImageHeaderText'>Welcome to Brew Beans</h1>
+            <h3 className='heroImageSubText'>- Experience the finest coffe</h3>
+            <Link className="buttonLink" to={`/shop`}>
+                <span className="buttonText">Order Now</span>
+            </Link>
+        </div>
+        
     )
 }
 
