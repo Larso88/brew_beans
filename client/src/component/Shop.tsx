c13233783d5b6ac2651ac067d4a30399a9ef4c6a
Lars Ole Algrøy <lars_ole_algroy@outlook.com>
2023-07-21 13:14:25 +0200

    - reworked the structure of the project and added server side.

--- client/src/component/Shop.tsx
+++ client/src/component/Shop.tsx
@@ -9,8 +9,8 @@ const Shop: FunctionComponent = () => {
         <div className='shopComponentWrapper'>
             <div className='heroImageWrapper'>
                 <img  className='heroImageShop' src={process.env.PUBLIC_URL + "/images/heroImageShop.jpg"} alt="" />
-                <h2 className='heroImageHeaderText'>Shop Brew Beans</h2>
-                <h3 className='HeroImageSubText'></h3>
+                <h2 className='shopHeroImageHeaderText'>Shop Brew Beans</h2>
+                <h3 className='shopHeroImageSubText'></h3>
             </div>
             <section className='shopSectionWrapper'>
                 {shopItems.map((item: ShopItem) => (
@@ -18,7 +18,7 @@ const Shop: FunctionComponent = () => {
                         <img className='shopCardImg' src={item.productImageUrl} alt={item.name} />
                         <h3 className='shopCardHeader'>{item.name}</h3>
                         <p className='shopCardSubText'>{item.subText}</p>
-                        <p>$ {item.price.toFixed(2)}</p>
+                        <p className='shopPriceText'>$ {item.price.toFixed(2)}</p>
                     </div>
                 ))}
             </section>
